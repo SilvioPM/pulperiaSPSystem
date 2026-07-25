5a16ab7398f2b29c004ada3f917074a460c00b2f
Silvio Jose Pantoja Mendoza <pantojamendozasilviojose@gmail.com>
2026-07-25 00:32:46 -0600

    Fix: service worker no cachea APIs y maneja errores

--- public/sw.js
+++ public/sw.js
@@ -1,14 +1,27 @@
-const CACHE = 'spsystem-v1'
-const urls = ['/', '/manifest.json', '/icon.svg']
+const CACHE = 'spsystem-v2'
+const STATIC = ['/', '/manifest.json', '/icon.svg']
 
 self.addEventListener('install', e => {
+  self.skipWaiting()
   e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(urls))
+    caches.open(CACHE).then(c => c.addAll(STATIC).catch(() => {}))
+  )
+})
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+    ))
   )
 })
 
 self.addEventListener('fetch', e => {
+  if (e.request.url.includes('/api/')) {
+    e.respondWith(fetch(e.request).catch(() => new Response('{}', { status: 502 })))
+    return
+  }
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+    caches.match(e.request).then(r => r || fetch(e.request).catch(() => new Response('Offline', { status: 503 })))
   )
 })
