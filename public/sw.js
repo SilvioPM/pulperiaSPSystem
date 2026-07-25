b6f151342518f74ae0d71e7ba0f2bdc5b1dde7b5
Silvio Jose Pantoja Mendoza <pantojamendozasilviojose@gmail.com>
2026-07-25 00:02:22 -0600

    Fix: agregar manifest.json y sw.js al repo (PWA)

---
+++ public/sw.js
@@ -0,0 +1,14 @@
+const CACHE = 'spsystem-v1'
+const urls = ['/', '/manifest.json', '/icon.svg']
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(urls))
+  )
+})
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  )
+})
